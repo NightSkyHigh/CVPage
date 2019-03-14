786b82622a8f963aa904304fa5313b84573d8084
“NightSkyHigh” <Thomas_95_@hotmail.com>
2019-03-14 14:16:01 +0100

    added files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const data = require('./info.json');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index', {data : data});
+});
+
+app.get('/data', (req, res) => {
+    res.json(data);
+});
+app.use('/images',express.static(__dirname + '/images'))
+app.listen(process.env.PORT || 8080)
